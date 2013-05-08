b4530962dbfadaf8aea8aaa39d3f0364b563dd74
John Henry <john@iamjohnhenry.com>
2013-05-07 19:45:20 -0700

    Added bounds to window

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ chrome.app.runtime.onLaunched.addListener(function() {
     'bounds': {
       'width': 600,
       'height': 700
-    }
+    },
+      'maxWidth': 600,
+      'maxHeight': 700
   });
 });
