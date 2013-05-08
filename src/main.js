c37f2b6b4c7daa68b41533d0431ecdac9c440d74
John Henry <john@iamjohnhenry.com>
2013-05-07 18:52:13 -0700

    Major restyling and interface rearrangement

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'bounds': {
       'width': 600,
-      'height': 800
+      'height': 700
     }
   });
 });
